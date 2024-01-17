9961f2bcb2767b02d8b8235b3ec5a13d381c2425
nurKaisar <proggamedev@gmail.com>
2024-01-17 20:40:57 +0600

    starting creating game

--- Source.cpp
+++ Source.cpp
@@ -10,7 +10,6 @@
 #include <glm/gtc/type_ptr.hpp>
 
 #include"res/stb_image.h"
-#include"res/linear_algebros.h"
 
 
 
@@ -102,35 +101,6 @@ static ShaderProgramSource ParseShaders(const std::string& filepath, const std::
 
 }
 
-std::vector<glm::vec3> vertices;
-
-void buildCircle(float radius, int vCount)
-{
-    float angle = 360.0f / vCount;
-
-    int triangleCount = vCount - 2;
-
-    std::vector<glm::vec3> temp;
-    // positions
-    for (int i = 0; i < vCount; i++)
-    {
-        float currentAngle = angle * i;
-        float x = radius * cos(glm::radians(currentAngle));
-        float y = radius * sin(glm::radians(currentAngle));
-        float z = 0.0f;
-
-        temp.push_back(glm::vec3(x, y, z));
-    }
-
-    for (int i = 0; i < triangleCount; i++)
-    {
-        vertices.push_back(temp[0]);
-        vertices.push_back(temp[i + 1]);
-        vertices.push_back(temp[i + 2]);
-    }
-}
-
-
 int main()
 {
     GLFWwindow* window;
@@ -159,36 +129,6 @@ int main()
     if (!glewInit() == GLEW_OK)
         return -1;
 
-    float vertices2[] =
-    {
-       -1.f,1.f,
-       -1.f,-1.0f,
-       1.0f,-1.0f,
-       -1.f,-1.0f,
-       1.0f,-1.0f,
-       1.0f,1.0f
-    };
-
-    unsigned int vbo;
-    unsigned int vao;
-    unsigned int vbo2;
-    unsigned int vao2;
-
-
-
-    GLCall(glGenVertexArrays(1, &vao));
-    GLCall(glBindVertexArray(vao));
-
-
-    GLCall(glGenBuffers(1, &vbo));
-    GLCall(glBindBuffer(GL_ARRAY_BUFFER, vbo));
-    GLCall(glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW));
-    
-
-    GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
-    GLCall(glEnableVertexAttribArray(0));
-
-    buildCircle(0.1, 128);
 
     ShaderProgramSource source = ParseShaders("res/Vertex_Shader.vertex", "res/Frag_Shader.frag");
 
@@ -216,46 +156,10 @@ int main()
     glDeleteShader(fragment_shader);
 
     glUseProgram(program);
-
-
-    GLCall(glGenVertexArrays(1, &vao2));
-    GLCall(glBindVertexArray(vao2));
-
-    GLCall(glGenBuffers(1, &vbo2));
-    GLCall(glBindBuffer(GL_ARRAY_BUFFER, vbo2));
-    GLCall(glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec3) * vertices.size(), &vertices[0], GL_STATIC_DRAW));
-
-
-    GLCall(glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (void*)0));
-    GLCall(glEnableVertexAttribArray(0));
-
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
- 
-    glm::mat4 model = glm::mat4(1.0f);
-    model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
-
-    glm::mat4 model2 = glm::mat4(1.0f);
-    model2 = glm::translate(model, glm::vec3(0.0f, 0.5f, 0.0f));
-
-    
  
     while (!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        glBindVertexArray(vao);
-        glUniform3fv(glGetUniformLocation(program, "Color"), 1, glm::value_ptr(glm::vec3(0.5f, 0.5f, 0.5f)));
-        GLCall(glUniformMatrix4fv(glGetUniformLocation(program, "model"), 1, GL_FALSE, glm::value_ptr(model)));
-        GLCall(glDrawArrays(GL_TRIANGLES, 0, 12));
-
-  
-        glBindVertexArray(vao2);
-        glUniform3fv(glGetUniformLocation(program, "Color"), 1, glm::value_ptr(glm::vec3(1.f, 1.f, 0.0f)));
-        GLCall(glUniformMatrix4fv(glGetUniformLocation(program, "model"), 1, GL_FALSE, glm::value_ptr(model2)));
-        GLCall(glDrawArrays(GL_TRIANGLES, 0, vertices.size()));
-
 
         glfwSwapBuffers(window);
 
@@ -263,7 +167,6 @@ int main()
         glfwPollEvents();
     }
 
-    glDeleteBuffers(1, &vbo);
 
     glfwTerminate();
     return 0;
