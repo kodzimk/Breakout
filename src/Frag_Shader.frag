a0d70eff565164ddb253445ab6ae59b255db1888
nurKaisar <proggamedev@gmail.com>
2024-01-21 17:28:14 +0600

    making game on process

--- src/Frag_Shader.frag
+++ src/Frag_Shader.frag
@@ -1,8 +1,11 @@
 #fragment
 #version 330 core
-out vec4 color;
+
+out vec4 FragColor;
+
+uniform vec4 color;
 
 void main()
 {
-    color = vec4(1.0f,1.0f,0.0f, 1.0f);
-}
+   FragColor = color;
+ };
--- src/Source.cpp
+++ src/Source.cpp
@@ -1,5 +1,44 @@
-#include"config.h"
+#include<GL/glew.h>
+#include<GLFW/glfw3.h>
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<sstream>
+#include<vector>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
+struct Object {
+    bool isAlive;
+    glm::mat4 model;
+    glm::vec3 position;
+    glm::vec4 color;
+};
+
+
+#define ASSERT(x) if(!(x)) __debugbreak();
+
+#define GLCall(x) GLClearError();\
+   x;\
+   ASSERT(GlLogCall(#x,__FILE__,__LINE__))
+
+static void GLClearError()
+{
+    while (glGetError() != GL_NO_ERROR);
+
+}
+
+static bool GlLogCall(const char* function, const char* file, int line)
+{
+    while (GLenum error = glGetError())
+    {
+        std::cout << "[OpenglError] (" << error << ")" << function << " " << file << ":" << line << std::endl;
+        return false;
+    }
+
+    return true;
+}
 
 #define HEIGHT 800
 #define WIDTH  1000
@@ -63,98 +102,168 @@ static ShaderProgramSource ParseShaders(const std::string& filepath, const std::
 
 int main()
 {
-    GLFWwindow* window;
-
-
+    // glfw: initialize and configure
+    // ------------------------------
+    glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    if (!glfwInit())
-        return -1;
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
 
-
-    window = glfwCreateWindow(WIDTH, HEIGHT, "Hello World", NULL, NULL);
-    if (!window)
+    // glfw window creation
+    // --------------------
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
     {
+        std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
 
-    glfwMakeContextCurrent(window);
+    ShaderProgramSource src = ParseShaders("src/Vertex_Shader.vertex", "src/Frag_Shader.frag");
+    const GLchar* vertexShaderSource = src.VertexSource.c_str();
+    const GLchar* fragmentShaderSource = src.FragmentSource.c_str();
+    std::cout << vertexShaderSource;
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
 
 
-    if (!glewInit() == GLEW_OK)
-        return -1;
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
 
-    float vertex[] = {
-      0.5f, -0.5f,   // bottom right
-    -0.5f, -0.5f,  // bottom left
-     0.0f,  0.5f
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    float vertices[] = {
+         0.5f,  0.5f, 0.0f,  // top right
+         0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left 
     };
-
-    unsigned int VAO,VBO;
-   
-
+    unsigned int indices[] = {  // note that we start from 0!
+        0, 1, 3,  // first Triangle
+        1, 2, 3   // second Triangle
+    };
+    unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    glGenBuffers(1, &EBO);
+
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertex), vertex, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glBindVertexArray(0);
+
+    Object blocks[38];
 
 
-    ShaderProgramSource source = ParseShaders("src/Vertex_Shader.vertex", "src/Frag_Shader.frag");
-    std::cout << source.FragmentSource;
-
-    const GLchar* vetrtex = source.VertexSource.c_str();
-    const GLchar* fragment = source.FragmentSource.c_str();
-
-    unsigned int program = glCreateProgram();
-
-    unsigned int vertex_shader, fragment_shader;
-
-    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    GLCall(glShaderSource(vertex_shader, 1, &vetrtex, nullptr));
-    GLCall(glCompileShader(vertex_shader));
-
-    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    GLCall(glShaderSource(fragment_shader, 1, &fragment, nullptr));
-    GLCall(glCompileShader(fragment_shader));
-
-    glAttachShader(program, vertex_shader);
-    glAttachShader(program, fragment_shader);
-
-    glLinkProgram(program);
-
-    glDeleteShader(vertex_shader);
-    glDeleteShader(fragment_shader);
+        blocks[0].isAlive = true;
+        blocks[0].position = { -4.5f,4.5f,0.0f };
+        blocks[0].model = glm::mat4(1.f);
+        blocks[0].model = glm::scale(blocks[0].model, glm::vec3(0.2f, 0.2f, 1.0f));
+        blocks[0].model = glm::translate(blocks[0].model, blocks[0].position);
+        blocks[0].color = glm::vec4(1.0f, 0.0f, 0.0f, 1.0f);
 
 
-    glUseProgram(program);
+        for (int i = 1; i < 19; i++)
+        {
+            blocks[i].isAlive = true;
+            blocks[i].position = { blocks[i-1].position.x + 0.5,blocks[i - 1].position.y,blocks[i - 1].position.z};
+            blocks[i].model = glm::mat4(1.f);
+            blocks[i].model = glm::scale(blocks[i].model, glm::vec3(0.2f, 0.2f, 1.0f));
+            blocks[i].model = glm::translate(blocks[i].model, blocks[i].position);
 
+            if(i % 2 == 0)
+            blocks[i].color = glm::vec4( 0.0f, i*0.3f,  0.0f,1.0f);
+
+            if (i % 3 == 0)
+                blocks[i].color = glm::vec4(i* 0.1f,  0.0f,  0.0f, 1.0f);
+
+            if (i % 5 == 0)
+                blocks[i].color = glm::vec4( 0.0f, 0.0f,  i *0.4f, 1.0f);
+        }
+
+        blocks[19].isAlive = true;
+        blocks[19].position = { -4.5f,3.5f,0.0f };
+        blocks[19].model = glm::mat4(1.f);
+        blocks[19].model = glm::scale(blocks[19].model, glm::vec3(0.2f, 0.2f, 1.0f));
+        blocks[19].model = glm::translate(blocks[19].model, blocks[19].position);
+        blocks[19].color = glm::vec4(1.0f, 0.0f, 0.0f, 1.0f);
+
+        for (int i = 20; i < 38; i++)
+        {
+            blocks[i].isAlive = true;
+            blocks[i].position = { blocks[i - 1].position.x + 0.5f,blocks[i - 1].position.y,blocks[i - 1].position.z };
+            blocks[i].model = glm::mat4(1.f);
+            blocks[i].model = glm::scale(blocks[i].model, glm::vec3(0.2f, 0.2f, 1.0f));
+            blocks[i].model = glm::translate(blocks[i].model, blocks[i].position);
+
+            if (i % 2 == 0)
+                blocks[i].color = glm::vec4(0.0f, i * 0.3f, 0.0f, 1.0f);
+
+            if (i % 3 == 0)
+                blocks[i].color = glm::vec4(i * 0.1f, 0.0f, 0.0f, 1.0f);
+
+            if (i % 5 == 0)
+                blocks[i].color = glm::vec4(0.0f, 0.0f, i * 0.4f, 1.0f);
+        }
 
     while (!glfwWindowShouldClose(window))
     {
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+     
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO); 
 
-        GLCall(glBindVertexArray(VAO));
-        GLCall(glDrawArrays(GL_TRIANGLES, 0, 3));
-
-
+        for (int i = 0; i < 38; i++)
+        {
+            GLCall(glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(blocks[i].model)));
+            GLCall(glUniform4f(glGetUniformLocation(shaderProgram, "color"),blocks[i].color.x,blocks[i].color.y,blocks[i].color.z,blocks[i].color.a));
+           
+            if(blocks[i].isAlive)
+              glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        }
+      
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    glDeleteProgram(shaderProgram);
+
     glfwTerminate();
     return 0;
-
 }
-
